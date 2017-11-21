6ba6e9db2aa6ae07e4e08067a03fada09dc733b8
Daria Zhuravel <zlakdanata@yandex.ru>
2017-11-21 20:58:48 +0300

    added .idea to gitignore

--- 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
@@ -36,4 +36,26 @@ public class Solution {
     }
 
     //add your code here - добавьте код тут
+    public static class ReadFileThread implements ReadFileInterface{
+
+        @Override
+        public void setFileName(String fullFileName) {
+
+        }
+
+        @Override
+        public String getFileContent() {
+            return null;
+        }
+
+        @Override
+        public void join() throws InterruptedException {
+
+        }
+
+        @Override
+        public void start() {
+
+        }
+    }
 }
