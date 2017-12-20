e12d828c7f0eede8a38941a500da0594b8bf95f6
Daria Zhuravel <zlakdanata@yandex.ru>
2017-12-20 16:48:27 +0300

    Solution task 19.15

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task19.task1915;
+
+/* 
+Дублируем текст
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+         testString.printSomething();
+
+        String result = outputStream.toString();
+        System.setOut(consoleStream);
+
+        StringBuilder stringBuilder = new StringBuilder(result);
+        System.out.println(stringBuilder);
+        FileWriter fileWriter = new FileWriter(fileName);
+        fileWriter.write(stringBuilder.toString());
+        fileWriter.close();
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
