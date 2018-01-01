5aed1c5fdb6aab850b406d047fd51ae00ef8bc4b
Daria Zhuravel <zlakdanata@yandex.ru>
2018-01-01 17:16:21 +0300

    Solution tasks modified folder out/

--- 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
@@ -27,7 +27,7 @@ public class Solution {
          map.put(8,"восемь");
          map.put(9,"девять");
          map.put(10,"десять");
-         map.put(11,"одиннацать");
+         map.put(11,"одиннадцать");
          map.put(12,"двенадцать");
      }
 
