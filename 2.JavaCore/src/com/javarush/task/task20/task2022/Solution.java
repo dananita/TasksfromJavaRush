4be9472d15c912ffe414250b1c8a799e7a9cc3f8
Daria Zhuravel <zlakdanata@yandex.ru>
2018-01-06 04:11:42 +0300

    Solution 20.22

--- 2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
@@ -6,10 +6,12 @@ import java.io.*;
 Переопределение сериализации в потоке
 */
 public class Solution implements Serializable, AutoCloseable {
-    private FileOutputStream stream;
+    private  transient FileOutputStream stream;
+    private String filename;
 
     public Solution(String fileName) throws FileNotFoundException {
         this.stream = new FileOutputStream(fileName);
+        this.filename = fileName;
     }
 
     public void writeObject(String string) throws IOException {
@@ -20,12 +22,13 @@ public class Solution implements Serializable, AutoCloseable {
 
     private void writeObject(ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
-        out.close();
+        //out.close();
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
-        in.close();
+        stream = new FileOutputStream(filename,true);
+        //in.close();
     }
 
     @Override
@@ -34,7 +37,10 @@ public class Solution implements Serializable, AutoCloseable {
         stream.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Solution solution = new Solution("/media/59A7E55F1637CB7E/SQL+GIT+JAVA/Example/15");
+        ObjectOutputStream oos = new ObjectOutputStream(solution.stream);
+        oos.writeObject(solution);
 
     }
 }
