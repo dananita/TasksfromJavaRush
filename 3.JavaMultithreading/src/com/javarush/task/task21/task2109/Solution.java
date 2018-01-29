dcb6779a1392c7d6353b041aacb4ccd1dd0f2144
Daria Zhuravel <zlakdanata@yandex.ru>
2018-01-29 13:40:19 +0300

    Solution task 21.09.Method Clone()

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task21.task2109;
+
+/* 
+Запретить клонирование
+*/
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            if (this.getClass().getName().contains("Solution$B"))
+            throw new CloneNotSupportedException();
+            return super.clone();
+        }
+    }
+
+    public static class C extends B {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        C c = new C (6,7,"Morry");
+        c.clone();
+        B b = new B(5,6,"Mary");
+        b.clone();
+    }
+}
