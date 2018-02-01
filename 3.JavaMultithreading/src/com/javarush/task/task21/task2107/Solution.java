0f4113a5c4a84332a522d27ba516949af366ee5a
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-01 18:47:40 +0300

    Solution task 21.07.Use Cloneable()

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task21.task2107;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* 
+Глубокое клонирование карты
+*/
+public class Solution implements Cloneable{
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.users.put("Hubert", new User(172, "Hubert"));
+        solution.users.put("Zapp", new User(41, "Zapp"));
+        Solution clone = null;
+        try {
+            clone = solution.clone();
+            System.out.println(solution);
+            System.out.println(clone);
+
+            System.out.println(solution.users);
+            System.out.println(clone.users);
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    protected Map<String, User> users = new LinkedHashMap();
+
+    @Override
+    protected Solution clone() throws CloneNotSupportedException {
+        Solution cloneSolution = new Solution();
+        for (Map.Entry<String, User> pair:users.entrySet()) {
+            cloneSolution.users.put(pair.getKey(),pair.getValue());
+        }
+        return cloneSolution;
+    }
+
+    public static class User implements Cloneable{
+        int age;
+        String name;
+
+        public User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            User clonUser = new User(this.age,this.name);
+            clonUser.age = this.age;
+            clonUser.name = this.name;
+            return clonUser;
+        }
+    }
+}
