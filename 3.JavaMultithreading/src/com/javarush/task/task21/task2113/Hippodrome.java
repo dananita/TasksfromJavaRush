c4fcc8c3eca9f0d5785193318d9170a18023b03c
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-04 03:08:28 +0300

    Solution 21.13.Hippodrome version 1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * @author Daria Zhuravel
+ * @author (zlakdanata @ yandex.ru)
+ * @date 04.02.18
+ **/
+public class Hippodrome {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * @author Daria Zhuravel
+ * @author (zlakdanata @ yandex.ru)
+ * @date 04.02.18
+ **/
+public class Horse {
+}
