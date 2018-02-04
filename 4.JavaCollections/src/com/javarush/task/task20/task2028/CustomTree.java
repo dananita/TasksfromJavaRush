7772c65161cf7bbeb059c0a52534b21ce9c485d4
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-04 13:43:10 +0300

    Solution 20.28.Tree (version 2)

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -2,12 +2,14 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.Collection;
 import java.util.List;
 
 /* 
 Построй дерево(1)
 */
 public class CustomTree extends AbstractList implements Cloneable, Serializable{
+
     public static void main(String[] args) {
         List<String> list = new CustomTree();
         for (int i = 1; i < 16; i++) {
@@ -18,13 +20,51 @@ public class CustomTree extends AbstractList implements Cloneable, Serializable{
         //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
     }
 
-    @Override
-    public Object get(int index) {
-        return null;
-    }
 
     @Override
     public int size() {
         return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        //return null;
+        throw new UnsupportedOperationException();
+    }
+
+
+    public String set(int index, String element) {
+        //return super.set(index, element);
+        throw new UnsupportedOperationException();
+    }
+
+
+    public void add(int index, String element) {
+        //super.add(index, element);
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index) {
+        //return super.remove(index);
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        //return super.addAll(index, c);
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        //return super.subList(fromIndex, toIndex);
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+       // super.removeRange(fromIndex, toIndex);
+        throw new UnsupportedOperationException();
     }
 }
