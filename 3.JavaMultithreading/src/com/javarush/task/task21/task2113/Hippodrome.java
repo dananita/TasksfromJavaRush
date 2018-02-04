eb8b01634f79e0897c355d86eae8737f10a99302
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-04 03:14:36 +0300

    Solution 21.13.Hippodrome version 2

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,12 +1,22 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Daria Zhuravel
  * @author (zlakdanata @ yandex.ru)
  * @date 04.02.18
  **/
 public class Hippodrome {
+    private List<Horse> horses;
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
     public static void main(String[] args) {
 
     }
+
 }
