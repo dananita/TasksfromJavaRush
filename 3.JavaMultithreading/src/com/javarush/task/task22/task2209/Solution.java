b8ccb6abf719c3bc964867cc1deff51a87b0ee42
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-12 14:04:48 +0300

    Solution 22.09.

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -1,9 +1,13 @@
 package com.javarush.task.task22.task2209;
 
-import java.awt.*;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /*
 Составить цепочку слов
@@ -12,17 +16,17 @@ public class Solution {
     public static void main(String[] args) {
         // /media/59A7E55F1637CB7E/SQL+GIT+JAVA/Example/changeWords
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        List <String> listWordsFromFile = new ArrayList<>();
-        try (Scanner scanner = new Scanner(new File(reader.readLine()))){
-            while (scanner.hasNext()){
+        List<String> listWordsFromFile = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             Scanner scanner = new Scanner(new File(reader.readLine()))) {
+            while (scanner.hasNext()) {
                 listWordsFromFile.add(scanner.next());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String [] words = new String[listWordsFromFile.size()];
-        for (int i = 0; i <words.length ; i++) {
+        String[] words = new String[listWordsFromFile.size()];
+        for (int i = 0; i < words.length; i++) {
             words[i] = listWordsFromFile.get(i);
         }
 
@@ -31,18 +35,46 @@ public class Solution {
     }
 
     public static StringBuilder getLine(String... words) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder resultStringBuilder = new StringBuilder();
+        StringBuilder[] tempFoundWord = new StringBuilder[words.length];
 
-        if (words.length==0){
-            stringBuilder.append("");
+        if (words.length == 0) {
+            resultStringBuilder.append("");
         } else {
-            List<String> list = new ArrayList<>();
-            Collections.addAll(list, words);
-            Collections.sort(list);
-            for (int i = 0; i <list.size() ; i++) {
-                stringBuilder.append(list.get(i)).append(" ");
+            Arrays.sort(words);
+            for (int i = 0; i < words.length; i++) {
+                tempFoundWord[i] = findTheNextWordWithTheLastLetter(i, words);
+                if (resultStringBuilder.length() < tempFoundWord[i].length()) {
+                    resultStringBuilder = tempFoundWord[i];
+                }
             }
         }
-        return stringBuilder;
+
+        return resultStringBuilder;
+    }
+
+    private static StringBuilder findTheNextWordWithTheLastLetter(int iWord, String[] words) {
+        StringBuilder str = new StringBuilder();
+        if (words != null) {
+            str.append(words[iWord]);
+            words[iWord] = "";
+            for (int i = 1; i < words.length; i++) {
+                for (int j = 1; j < words.length; j++) {
+                    if (!words[j].equals("")) {
+
+                        if (Character.toLowerCase(words[j].charAt(0)) ==
+                                Character.toLowerCase(str.charAt(str.length() - 1))) {
+                            str.append(" " + words[j]);
+                            words[j] = "";
+                        } else if (Character.toLowerCase(words[j].charAt(words[j].length() - 1)) ==
+                                Character.toLowerCase(str.charAt(0))) {
+                            str.insert(0, words[j] + " ");
+                            words[j] = "";
+                        }
+                    }
+                }
+            }
+        }
+        return str;
     }
 }
