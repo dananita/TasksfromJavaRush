8ef12b91eb0596dfefbf62738b1be4ef65c091b3
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-14 00:07:34 +0300

    Solution 23.12.Snake (7)

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,9 +1,31 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.List;
+
 /**
  * @author Daria Zhuravel
  * @author (zlakdanata@yandex.ru)
  * @date 13.02.18
  **/
 public class Snake {
+    private List<SnakeSection> sections;
+    private boolean isAlive;
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+
+    private SnakeDirection direction;
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
 }
