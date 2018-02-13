b96224900ba2bbc2a619566a786b29daeefa2708
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-14 00:17:39 +0300

    Solution 23.12.Snake (8)

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,12 +11,33 @@ import java.util.List;
 public class Snake {
     private List<SnakeSection> sections;
     private boolean isAlive;
+    private SnakeDirection direction;
+
+    public Snake (int x, int y){
+        sections = new ArrayList<>();
+        sections.add(new SnakeSection(x,y));
+        setAlive(true);
+    }
+
+    public int getX(){
+        return sections.get(0).getX();
+    }
+
+    public int getY(){
+        return sections.get(0).getY();
+    }
+
+    public void move(){
+
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 
     public void setDirection(SnakeDirection direction) {
         this.direction = direction;
     }
-
-    private SnakeDirection direction;
 
     public List<SnakeSection> getSections() {
         return sections;
