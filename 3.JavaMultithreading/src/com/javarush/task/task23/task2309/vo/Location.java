817b1f32ef8b456a9847178bab5c131f07829dde
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-17 04:44:26 +0300

    Solution 23.09.Anonymous class

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/vo/Location.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task23.task2309.vo;
+
+/**
+ * @author Daria Zhuravel
+ * @author (zlakdanata @ yandex.ru)
+ * @date 17.02.18
+ **/
+public class Location extends NamedItem{
+}
