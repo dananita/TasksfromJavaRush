b4a7e0ddbda958f9dcfee0f8e305135588811ee2
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-18 02:55:50 +0300

    Solution 24.13.Arkanoid (1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task24.task2413;
+
+/**
+ * @author Daria Zhuravel
+ * @author (zlakdanata@yandex.ru)
+ * @date 18.02.18
+ **/
+public class Arkanoid {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task24.task2413;
+
+/**
+ * @author Daria Zhuravel
+ * @author (zlakdanata@yandex.ru)
+ * @date 18.02.18
+ **/
+public class Ball {
+}
