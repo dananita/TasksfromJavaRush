270770e38fc57968abe5ccaf220d2e8d6d748be4
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-19 00:54:35 +0300

    Solution 24.13.Arkanoid (13)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -6,6 +6,40 @@ package com.javarush.task.task24.task2413;
  * @date 18.02.18
  **/
 public class Ball extends BaseObject {
+    private double speed;
+    private double direction;
+    private double dx;
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        this.dx = direction/speed;
+        this.dy = direction/speed;
+        isFrozen = true;
+    }
+
+    private double dy;
+    private  boolean isFrozen;
+
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+
     public Ball(double x, double y, double radius) {
         super(x,y,radius);
     }
