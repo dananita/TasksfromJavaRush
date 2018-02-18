9ae24071da285123aa7c1abc959f096c79c3d81d
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-18 03:02:04 +0300

    Solution 24.13.Arkanoid (2)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -6,7 +6,26 @@ package com.javarush.task.task24.task2413;
  * @date 18.02.18
  **/
 public class Arkanoid {
-    public static void main(String[] args) {
+     private int width;
+     private int height;
 
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        
     }
 }
