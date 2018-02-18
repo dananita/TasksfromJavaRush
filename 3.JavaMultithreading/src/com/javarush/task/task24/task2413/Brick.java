84a8179027c9fbbc547a76d04e95508be1bf8825
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-19 00:47:29 +0300

    Solution 24.13.Arkanoid (12)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -9,6 +9,9 @@ public class Brick extends BaseObject{
     public Brick(double x, double y, double radius) {
         super(x, y, radius);
     }
+    public Brick(double x, double y) {
+        super(x, y, 3);
+    }
 
     @Override
     public void draw(Canvas canvas) {
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -16,15 +16,14 @@ public class Canvas {
         setMatrix(new char[height][width]);
     }
 
-    public void print() {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+    public  void print(){
+        for (int i = 0; i <matrix.length ; i++) {
+            for (int j = 0; j <matrix[0].length ; j++) {
                 System.out.print(matrix[i][j]);
             }
             System.out.println();
         }
     }
-
     public void setPoint(double x, double y, char c) {
         if (x < 0 || y < 0 || y > matrix.length || x > matrix[0].length) {
 
