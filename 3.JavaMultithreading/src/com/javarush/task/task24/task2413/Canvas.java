62b29a7201f62414b4c727e10cc3254e7bf6f5f3
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-18 22:38:50 +0300

    Solution 24.13.Arkanoid (9)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -6,4 +6,38 @@ package com.javarush.task.task24.task2413;
  * @date 18.02.18
  **/
 public class Canvas {
+    private  int width;
+    private int height;
+    private char matrix [][];
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height+2][width+2];
+    }
+
 }
