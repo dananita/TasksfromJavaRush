543290f6eca48851b023f4a8890e1c4a37e6a43f
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-18 23:43:45 +0300

    Solution 24.13.Arkanoid (11)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -10,6 +10,21 @@ public class Canvas {
     private int height;
     private char matrix [][];
 
+    public void clear() {
+        int height = getHeight() + 2;
+        int width = getWidth() + 2;
+        setMatrix(new char[height][width]);
+    }
+
+    public void print() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     public void setPoint(double x, double y, char c) {
         if (x < 0 || y < 0 || y > matrix.length || x > matrix[0].length) {
 
