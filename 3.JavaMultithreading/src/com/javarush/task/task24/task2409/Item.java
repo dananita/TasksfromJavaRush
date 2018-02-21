885a2918c0275f4d6590ac65b80aa503533a9f37
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-21 11:17:24 +0300

    Solution 24.09.Jeans shop

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * @author Daria Zhuravel
+ * @author (zlakdanata@yandex.ru)
+ * @date 21.02.18
+ **/
+public interface Item {
+    int getId();
+    double getPrice();
+    String getTM();
+
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * @author Daria Zhuravel
+ * @author (zlakdanata@yandex.ru)
+ * @date 21.02.18
+ **/
+public interface Jeans extends Item{
+    int getLength();
+    int getSize();
+}
