1a4f730f4ee17f1c36ea81ce50d152ea95aaad72
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-22 17:54:47 +0300

    Solution 36.08.Simple MVC without a real DB

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task36.task3608.model;
+
+/**
+ * @author Daria Zhuravel
+ * @author (zlakdanata@yandex.ru)
+ * @date 22.02.18
+ **/
+public interface Model {
+    ModelData getModelData();
+
+    void loadUsers();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.List;
+
+/**
+ * @author Daria Zhuravel
+ * @author (zlakdanata@yandex.ru)
+ * @date 22.02.18
+ **/
+public class ModelData {
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
