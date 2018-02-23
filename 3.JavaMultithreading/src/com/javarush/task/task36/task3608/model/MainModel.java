05a626a4adb2b0a744db7d4e1d94640c51146a0b
Daria Zhuravel <zlakdanata@yandex.ru>
2018-02-24 01:07:40 +0300

    Solution 36.08 (4).Simple MVC without a real DB

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
+
+/**
+ * @author Daria Zhuravel
+ * @author (zlakdanata@yandex.ru)
+ * @date 24.02.18
+ **/
+public class MainModel implements Model{
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public ModelData getModelData() {
+        return this.modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(userService.getUsersBetweenLevels(1,100));
+    }
+}
