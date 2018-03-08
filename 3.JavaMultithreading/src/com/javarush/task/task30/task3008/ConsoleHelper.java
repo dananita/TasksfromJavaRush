27b82f8b2749ad3915e678d25fb2b21e09d3b879
Daria Zhuravel <zlakdanata@yandex.ru>
2018-03-09 02:54:01 +0300

    Solution 30.08.Chat on server-client base (2)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -5,8 +5,49 @@ package com.javarush.task.task30.task3008;
  * @date 09.03.18
  **/
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * вспомогательный класс, для чтения или записи в консоль.
  */
 public class ConsoleHelper {
+    private static BufferedReader bufferedReader =
+            new BufferedReader(new InputStreamReader(System.in));
+
+    /**
+     * Выводит message на консоль
+     * @param message
+     */
+    public static void writeMessage( String message){
+        System.out.println(message);
+    }
+
+    public static String readString(){
+        String result = null;
+        while(true) {
+            try {
+                result = bufferedReader.readLine();
+                break;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return result;
+    }
+    public static int readInt(){
+        int result = 0;
+        while(true) {
+            try {
+                result = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException ex) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return result;
+    }
+
+
 }
