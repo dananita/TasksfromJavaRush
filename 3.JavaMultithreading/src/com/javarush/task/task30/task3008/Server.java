c905273aad2987c0abb947d6d97e9f37d629c539
Daria Zhuravel <zlakdanata@yandex.ru>
2018-03-10 01:09:55 +0300

    Solution 30.08.Chat on server-client base (6)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -6,6 +6,7 @@ package com.javarush.task.task30.task3008;
  **/
 
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -13,15 +14,34 @@ import java.net.Socket;
  */
 public class Server {
     public static void main(String[] args) {
-        try {
-            Socket a = new Socket();
-            Connection connection = new Connection(a);
-            connection.send(new Message(MessageType.TEXT,"maria"));
-            System.out.println(connection.receive().getData());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        int portSocket = ConsoleHelper.readInt();
+        ServerSocket serverSocket = null;
+            try {
+                serverSocket = new ServerSocket(portSocket);
+                Socket fromClient;
+                System.out.println("Server running");
+                while(true) {
+                    fromClient = serverSocket.accept();
+                    Handler handler = new Handler(fromClient);
+                    handler.start();
+                }
+            } catch (IOException e) {
+                System.out.println("ServerSocket has an error!"+ e.getMessage());
+                try {
+                    if (serverSocket != null && !serverSocket.isClosed()) {
+                        serverSocket.close();
+                    }
+                } catch (IOException e1) {
+                    //* NOP
+                }
+            }
+    }
+
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
         }
     }
 }
