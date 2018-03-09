8d1256ac00038dba486a56a98f4ad1529d2ff674
Daria Zhuravel <zlakdanata@yandex.ru>
2018-03-10 00:43:01 +0300

    Solution 30.08.Chat on server-client base (5)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -5,11 +5,23 @@ package com.javarush.task.task30.task3008;
  * @date 09.03.18
  **/
 
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * Основной класс сервера
  */
 public class Server {
     public static void main(String[] args) {
-        
+        try {
+            Socket a = new Socket();
+            Connection connection = new Connection(a);
+            connection.send(new Message(MessageType.TEXT,"maria"));
+            System.out.println(connection.receive().getData());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
