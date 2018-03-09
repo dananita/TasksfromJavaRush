b03f5e0e7a945f0679aff1f1c3afabccc43913f3
Daria Zhuravel <zlakdanata@yandex.ru>
2018-03-10 01:36:37 +0300

    Solution 30.08.Chat on server-client base (7)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -8,11 +8,34 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Основной класс сервера
  */
 public class Server {
+    private  static Map<String,Connection> connectionMap = new ConcurrentHashMap<>();
+
+    /**
+     * отправляет сообщение всем соединениям из connectionMap
+     * @param message
+     */
+    public static void sendBroadcastMessage(Message message){
+
+        Iterator<String> ite = connectionMap.keySet().iterator();
+        while(ite.hasNext()){
+            String key = ite.next();
+            Connection connectionWithClient = connectionMap.get(key);
+            try {
+                connectionWithClient.send(message);
+            } catch (IOException e) {
+                System.out.println("Don't worry, but The message is not send");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int portSocket = ConsoleHelper.readInt();
         ServerSocket serverSocket = null;
