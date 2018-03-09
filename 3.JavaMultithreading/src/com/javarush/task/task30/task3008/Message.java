71fe42da7bc6fbae7c9e2121153ba40309972d96
Daria Zhuravel <zlakdanata@yandex.ru>
2018-03-09 03:04:03 +0300

    Solution 30.08.Chat on server-client base (4)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Message.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Message.java
@@ -5,8 +5,30 @@ package com.javarush.task.task30.task3008;
  * @date 09.03.18
  **/
 
+import java.io.Serializable;
+
 /**
  * класс, отвечающий за пересылаемые сообщения
  */
-public class Message {
+public class Message implements Serializable {
+    private final MessageType type;
+    private final String data;
+
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public Message(MessageType type) {
+        this.type = type;
+        this.data = null;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
 }
