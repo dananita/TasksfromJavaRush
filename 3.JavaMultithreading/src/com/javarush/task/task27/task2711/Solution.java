c7885f299157c75229b367c07493f51f8c72efb0
Daria Zhuravel <zlakdanata@yandex.ru>
2018-03-22 16:59:29 +0300

    Solution 27.11. CountDownLantch

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task27.task2711;
+
+import java.util.concurrent.CountDownLatch;
+
+/* 
+CountDownLatch
+*/
+public class Solution {
+    //private final Object lock = new Object();
+    //private volatile boolean isWaitingForValue = true;
+
+    CountDownLatch latch = new CountDownLatch(1);
+
+
+    public void someMethod() throws InterruptedException {
+            retrieveValue();
+            latch.countDown();
+            latch.await();
+    }
+
+    void retrieveValue() {
+        System.out.println("Value retrieved.");
+    }
+
+    public static void main(String[] args) {
+    Solution solution = new Solution();
+        try {
+            solution.someMethod();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
