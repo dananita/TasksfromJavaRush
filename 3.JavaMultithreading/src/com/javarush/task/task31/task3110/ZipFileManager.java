c954348ebb753a970a8e9521d16cfeaa77aafdad
Daria Zhuravel <zlakdanata@yandex.ru>
2018-03-22 19:15:07 +0300

    Solution 31.10.Archiver(2)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -5,7 +5,11 @@ package com.javarush.task.task31.task3110;
  * @date 22.03.18
  **/
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Класс будет совершать операции над файлом архива
@@ -19,6 +23,16 @@ public class ZipFileManager {
     }
 
     public void createZip (Path source) throws Exception{
-
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            String fileNameSource = source.getFileName().toString();
+            ZipEntry zipEntry = new ZipEntry(fileNameSource);
+                zipOutputStream.putNextEntry(zipEntry);
+                try (
+                        InputStream inputStream = Files.newInputStream(source)) {
+                    while (inputStream.available() > 0) {
+                        zipOutputStream.write(inputStream.read());
+                    }
+                }
+        }
     }
 }
