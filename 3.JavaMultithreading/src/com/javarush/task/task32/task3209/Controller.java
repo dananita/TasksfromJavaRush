eb28e9166d30127abe2cbefbb1d8584785658078
Daria Zhuravel <zlakdanata@yandex.ru>
2018-04-01 22:14:00 +0300

    Solution 32.09.HTML Editor(15)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.event.DocumentListener;
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 /**
@@ -35,4 +37,14 @@ public class Controller {
     public HTMLDocument getDocument() {
         return document;
     }
+
+    public void  resetDocument(){
+        if (document != null) {
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(view.getUndoListener());
+        view.update();
+    }
+
 }
