3136592861d22f4376ce3c2672aac3ea5cbc0a11
Daria Zhuravel <zlakdanata@yandex.ru>
2018-04-02 15:22:42 +0300

    Solution 20.28.(4_ +)

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -2,44 +2,42 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /* 
 Построй дерево(1)
 */
-public class CustomTree extends AbstractList<String> implements Cloneable, Serializable{
-    Entry <String> root  = new Entry<>("0");
 
+public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+    Entry<String> root = new Entry<>("0");
 
-    static class Entry<T> implements Serializable{
-        String  elementName;
+    static class Entry<T> implements Serializable {
+
+        String elementName;
         int lineNumber;
         boolean availableToAddLeftChildren, availableToAddRightChildren;
         Entry<T> parent, leftChild, rightChild;
 
-
-
-
         public Entry(String elementName) {
             this.elementName = elementName;
-            availableToAddLeftChildren = true;
-            availableToAddRightChildren = true;
+            this.availableToAddLeftChildren = true;
+            this.availableToAddRightChildren = true;
         }
 
-
-        void checkChildren(){
-            if(leftChild!=null){
+        void checkChildren() {
+            if (leftChild != null) {
                 availableToAddLeftChildren = false;
             }
-            if(rightChild!= null){
+            if (rightChild != null) {
                 availableToAddRightChildren = false;
             }
         }
 
-        boolean isAvailableToAddChildren(){
-            return availableToAddLeftChildren||availableToAddRightChildren;
+        boolean isAvailableToAddChildren() {
+            return availableToAddLeftChildren | availableToAddRightChildren;
         }
 
-    }
+    } // End of Entry class
 
 
     public static void main(String[] args) {
@@ -48,72 +46,140 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
             list.add(String.valueOf(i));
         }
 
-        System.out.println("Size: Expected 15,actual is "+list.size());
-        System.out.println("Parent: Expected 3, actual is " + ((CustomTree) list).getParent("8"));
-        System.out.println("Parent: Expected 1, actual is " + ((CustomTree) list).getParent("3"));
-        System.out.println("Parent: Expected 7, actual is " + ((CustomTree) list).getParent("15"));
-       // list.remove("15");
-        System.out.println("Size: Expected 14, actual is " + list.size());
-        System.out.println("Parent: Expected null, actual is " + ((CustomTree) list).getParent("11"));
+        //System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
+        list.remove("5");
+        //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
     }
 
-    public boolean add(String s){
-        boolean result = false;
 
-        if(s != null) {
-            Entry<String> rootElement = new Entry<>(s);
-            Queue<Entry<String>> queue = new LinkedList<>();
-            queue.add(root);
-            boolean isOperationAdd = true;
-
-
-            while (isOperationAdd && !queue.isEmpty()) {
-                Entry<String> currentElement = queue.remove();
-
-                if(!currentElement.availableToAddLeftChildren&& currentElement.leftChild!=null){
-                    queue.add(currentElement.leftChild);
-                } else {
-                    if (isOperationAdd && currentElement.availableToAddLeftChildren){
-                        rootElement.parent= currentElement;
-                        currentElement.leftChild = rootElement;
-                        isOperationAdd = false;
-                        currentElement.checkChildren();
-                    }
+    @Override
+    public boolean add(String s) {
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Entry<String> currentNode = queue.poll();
+            currentNode.checkChildren();
+            if (currentNode.isAvailableToAddChildren()) {
+                if (currentNode.availableToAddLeftChildren) {
+                    currentNode.leftChild = new Entry<>(s);
+                    currentNode.leftChild.parent = currentNode;
+                    return true;
+                } else if (currentNode.availableToAddRightChildren) {
+                    currentNode.rightChild = new Entry<>(s);
+                    currentNode.rightChild.parent = currentNode;
+                    return true;
                 }
-
-                if(!currentElement.availableToAddRightChildren&& currentElement.rightChild!=null){
-                    queue.add(currentElement.rightChild);
-                } else {
-                    if (isOperationAdd && currentElement.availableToAddRightChildren){
-                        rootElement.parent= currentElement;
-                        currentElement.rightChild = rootElement;
-                        isOperationAdd = false;
-                        currentElement.checkChildren();
-                    }
+            } else {
+                if (currentNode.leftChild != null) {
+                    queue.offer(currentNode.leftChild);
+                }
+                if (currentNode.rightChild != null) {
+                    queue.offer(currentNode.rightChild);
                 }
             }
         }
-        return result;
+        return false;
     }
 
-    public String getParent(String s){
+    @Override
+    public String get(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String set(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, String s) {
+
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+
+        int size = -1;
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Entry<String> currentNode = queue.poll();
+            size++;
+            if (currentNode.leftChild != null) {
+                queue.offer(currentNode.leftChild);
+            }
+            if (currentNode.rightChild != null) {
+                queue.offer(currentNode.rightChild);
+            }
+        }
+        return size;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        String s = (String) o;
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Entry<String> currentNode = queue.poll();
+            if (currentNode.leftChild != null) {
+                if (currentNode.leftChild.elementName.equals(s)) {
+                    currentNode.leftChild = null;
+                    return true;
+                }
+                queue.offer(currentNode.leftChild);
+            }
+            if (currentNode.rightChild != null) {
+                if (currentNode.rightChild.elementName.equals(s)) {
+                    currentNode.rightChild = null;
+                    return true;
+                }
+                queue.offer(currentNode.rightChild);
+            }
+        }
+        return false;
+    }
+
+    public String getParent(String s) {
         String parentName = null;
-        if(root!=null||s!=null||!s.equals(root.elementName)){
-            Entry <String> rootElement = new Entry<>(s);
+        if (root != null || s != null || !s.equals(root.elementName)) {
+            Entry<String> rootElement = new Entry<>(s);
             Queue<Entry<String>> queue = new LinkedList<>();
             queue.offer(root);
 
-            while(parentName==null && !queue.isEmpty()){
+            while (parentName == null && !queue.isEmpty()) {
                 Entry<String> currentElement = queue.remove();
-                if(currentElement.leftChild != null){
-                    if (!currentElement.leftChild.elementName.equals(s)){
+                if (currentElement.leftChild != null) {
+                    if (!currentElement.leftChild.elementName.equals(s)) {
                         queue.add(currentElement.leftChild);
                     } else {
                         parentName = currentElement.elementName;
                     }
                 }
-                if(currentElement.rightChild != null){
-                    if (!currentElement.rightChild.elementName.equals(s)){
+                if (currentElement.rightChild != null) {
+                    if (!currentElement.rightChild.elementName.equals(s)) {
                         queue.add(currentElement.rightChild);
                     } else {
                         parentName = currentElement.elementName;
@@ -122,93 +188,5 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
             }
         }
         return parentName;
-    }
-
-    @Override
-    public int size() {
-        Queue<Entry<String>> queue = new LinkedList<>();
-        int count = 1;
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            Entry<String> currentEntry = queue.remove();
-            if (!currentEntry.availableToAddRightChildren && currentEntry.rightChild != null) {
-                count++;
-                queue.add(currentEntry.rightChild);
-            }
-            if (!currentEntry.availableToAddLeftChildren && currentEntry.leftChild != null) {
-                count++;
-                queue.add(currentEntry.leftChild);
-            }
-        }
-        return count;
-    }
-
-    @Override
-    public String get(int index) {
-        //return null;
-        throw new UnsupportedOperationException();
-    }
-
-
-    public String set(int index, String element) {
-        //return super.set(index, element);
-        throw new UnsupportedOperationException();
-    }
-
-    public void add(int index, String element) {
-        //super.add(index, element);
-        throw new UnsupportedOperationException();
-    }
-
-    public  void remove(String s){
-        Queue<Entry<String>> queue = new LinkedList<>();
-        queue.add(root);
-        boolean isOperationRemove = false;
-        while (!isOperationRemove && !queue.isEmpty()) {
-            Entry<String> currentElement = queue.remove();
-            if (!currentElement.leftChild.elementName.equals(s)) {
-                queue.add(currentElement.leftChild);
-            } else {
-                if (!isOperationRemove) {
-                    currentElement.leftChild = null;
-                    isOperationRemove = true;
-                }
-            }
-            if (!currentElement.rightChild.elementName.equals(s)) {
-                queue.add(currentElement.rightChild);
-            } else {
-                if (!isOperationRemove) {
-                    currentElement.rightChild = null;
-                    isOperationRemove = true;
-                }
-            }
-        }
-    }
-
-    public boolean remove (Object o){
-        throw new UnsupportedOperationException();
-    }
-    @Override
-    public String remove(int index) {
-        //return super.remove(index);
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addAll(int index, Collection c) {
-        //return super.addAll(index, c);
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public List subList(int fromIndex, int toIndex) {
-        //return super.subList(fromIndex, toIndex);
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    protected void removeRange(int fromIndex, int toIndex) {
-       // super.removeRange(fromIndex, toIndex);
-        throw new UnsupportedOperationException();
     }
 }
