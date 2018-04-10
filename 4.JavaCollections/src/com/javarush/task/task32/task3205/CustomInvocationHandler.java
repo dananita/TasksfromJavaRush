a30118bebfd2ffbc7097435a15da85c3113d1ecc
Daria Zhuravel <zlakdanata@yandex.ru>
2018-04-10 16:52:33 +0300

    Solution 33.12.JSON

--- 4.JavaCollections/src/com/javarush/task/task32/task3205/CustomInvocationHandler.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3205/CustomInvocationHandler.java
@@ -1,9 +1,24 @@
 package com.javarush.task.task32.task3205;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
 /**
  * @author Daria Zhuravel
- * @author (zlakdanata @ yandex.ru)
  * @date 09.04.18
  **/
-public class CustomInvocationHandler {
+public class CustomInvocationHandler implements InvocationHandler {
+    private SomeInterfaceWithMethods someInterfaceWithMethods;
+
+    public CustomInvocationHandler(SomeInterfaceWithMethods someInterfaceWithMethods) {
+        this.someInterfaceWithMethods = someInterfaceWithMethods;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println(method.getName() + " in");
+        Object result = method.invoke(someInterfaceWithMethods,args);
+        System.out.println(method.getName() + " out");
+        return result;
+    }
 }
