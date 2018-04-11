0260f5e1a99d69dcdd7b6954e2a295491e3fda9f
Daria Zhuravel <zlakdanata@yandex.ru>
2018-04-11 17:03:07 +0300

    Solution 22.13

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * @author Daria Zhuravel
+ * @date 11.04.18
+ **/
+public class Field {
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * @author Daria Zhuravel
+ * @date 11.04.18
+ **/
+public class Tetris {
+    public static void main(String[] args) {
+
+    }
+}
