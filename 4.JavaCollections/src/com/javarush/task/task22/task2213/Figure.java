a977be8a884f36b2ffb87701668896ae4ab78fee
Daria Zhuravel <zlakdanata@yandex.ru>
2018-04-11 17:22:54 +0300

    Solution 22.13.Tetris (8)

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -5,4 +5,25 @@ package com.javarush.task.task22.task2213;
  * @date 11.04.18
  **/
 public class Figure {
+    private int x;
+    private int y;
+    private int matrix[][];
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
